87fcad9953bf09e69cbcfed1be43c01b8739c7a1
A23AnxoPF <A23AnxoPF@a26eq29.sanclemente.local>
2024-02-06 13:56:25 +0100

    ej 1 IF

---
+++ U04/src/sentenciaIF/Ej1_B.java
@@ -0,0 +1,26 @@
+package sentenciaIF;
+
+import java.util.Scanner;
+	
+public class Ej1_B {
+
+	public static void main(String[] args) {
+	 
+		Scanner entrada=new Scanner(System.in);
+		 int x;
+		 int y;
+		 System.out.println("Teclea número entero x");
+		 x=entrada.nextInt();
+		 System.out.println("Teclea número entero y");
+		 y=entrada.nextInt();
+		 
+		 if (x== 10) {
+		 System.out.println("x vale 10");
+		 System.out.println("x + y vale: " + (x+y));
+		 }
+		 else
+		 System.out.println("x NO vale 10");
+	 	}
+	}
+
+
