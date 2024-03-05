d972aa6eefb968467e7b7971e0bb345271b8d8ae
A23AnxoPF <A23AnxoPF@a26eq29.sanclemente.local>
2024-03-05 14:27:00 +0100

    Ej1 Recursividad

--- U04/src/recursividad/Ej1Funcion.java
+++ U04/src/recursividad/Ej1Funcion.java
@@ -1,5 +1,14 @@
 package recursividad;
 
 public class Ej1Funcion {
-
+	int b = 1;
+	int factorial(int n) {
+		if(n==0)
+			return 1;
+		if(n>0)
+			for (int i = 1; i <= n; i++) {
+	            b = b * i;
+	        }
+			return b;
+	}
 }
--- U04/src/recursividad/Ej1Main.java
+++ U04/src/recursividad/Ej1Main.java
@@ -1,8 +1,14 @@
 package recursividad;
 
+import java.util.Scanner;
+
 public class Ej1Main {
 	public static void main(String[] args) {
-		
+		Ej1Funcion f = new Ej1Funcion();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Teclea número entero para calcular factorial: ");
+		int n = sc.nextInt();
+		System.out.println("Su factorial es: "+f.factorial(n));
 	}
 }
 	
