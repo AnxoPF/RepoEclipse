a9fc8739a8998a910a08536cdfcf2e6a8662b82f
A23AnxoPF <A23AnxoPF@a26eq29.sanclemente.local>
2024-03-06 10:27:05 +0100

    inicio arrays

---
+++ U05/src/unidimensionales/Prueba.java
@@ -0,0 +1,12 @@
+package unidimensionales;
+
+public class Prueba {
+	public static void main(String[] args) {
+		
+	String [] dias_semana = {"Lunes", "Martes", "Miércoles", "Viernes"};
+	System.out.println(dias_semana[3]); //lectura
+	
+	int [] lista = new int[10];
+	System.out.println (dias_semana.length);
+	}
+}
